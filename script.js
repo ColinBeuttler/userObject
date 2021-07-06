ea51fe8d82737677b63a054af5a1499c8b733c81
Colin <beutt102@mail.chapman.edu>
2021-07-06 14:55:47 -0700

    objwork

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ const submitEl = document.querySelector('.submitBtn')
 const userNameEl = document.querySelector('.userName--Input')
 const userPasswordEl = document.querySelector('.userPassword--Input')
 
+const user ={
+    user: 'Colin',
+    password : '7339'
+}
+
+
+
 // object creation
 class UserObj {
     constructor(userName, userPassword){
@@ -13,6 +20,22 @@ class UserObj {
         this.userPassword = userPassword
     }
 }
+
+// app functions
+
+class App {
+     #userObj = []
+
+     constructor(){
+         form.addEventListener('submit', this._userObj.bind(this))
+
+     }
+}
+
+// const userName = JSON.stringify(userNameEl.value)
+// const userPassword = JSON.stringify(userPasswordEl.value)
+
+console.log(user)
 
 // submit element
 
