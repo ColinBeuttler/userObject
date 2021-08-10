f2e70a90e0b2651bac92c450b7ad7c9ceec4ec91
Colin <beutt102@mail.chapman.edu>
2021-08-10 11:40:18 -0700

    codecleanup

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// html elements
+// HTML elements
 const formEl = document.querySelector('.userForm');
 const submitEl = document.querySelector('.submitBtn');
 const loginEl = document.querySelector('.loginBtn');
@@ -12,7 +12,7 @@ const regNewUserEl = document.querySelector('.regNewUser');
 const existingUserEl = document.querySelector('.existingUser');
 
 
-
+// User Array
 let users = [];
 
 users.push(JSON.parse(localStorage.getItem('accounts')));
@@ -24,81 +24,6 @@ users.push(JSON.parse(localStorage.getItem('accounts')));
 
 // let existingUsers = JSON.parse(localStorage.getItem(users));
 // existingUser = existingUser ? existingUser.split(',') : [];
-
-// Object Prototypes
-
-class UserObj {
-  constructor(){
-  this.userEmail = document.querySelector('.userEmail--Input').value 
-  this.userName = document.querySelector('.userName--Input').value
-  this.userPassword = document.querySelector('.userPassword--Input').value
-}
-}
-
-// Button Methods
-
-// Login existing user/ check for account
-
-const checkAccLogin = function() {
-  formEl.remove();
-  console.log('logged in');
-  headerEl.append(`\n Welcome Back`);
-}
-
-// create User Object
-
-const pushObj = function() {
-  // user oject created
-  const newAccount = new UserObj()
-  // pushed to user array
-  users.push(newAccount);
-  formEl.remove();
-  headerEl.append(`\n Welcome ${newAccount.userName}`);
-  console.log('added', { newAccount });
-  localStorage.setItem('accounts', JSON.stringify(users))
-};
-
-// Initate New User
-
-// Hide/Show display CSS display 
-const newUserinit = function(){ 
-    document.querySelector('.userEmailDiv').style.display = "block"
-    document.querySelector('.loginBtn').style.display ="none"
-    document.querySelector('.submitBtn').style.display ="inline-block"
-    document.querySelector('.regNewUser').style.display ="none"
-    document.querySelector('.existingUser').style.display ="block"
-}
-
-const existingLogin = function(){
-    document.querySelector('.userEmailDiv').style.display = "none"
-    document.querySelector('.loginBtn').style.display ="inline-block"
-    document.querySelector('.submitBtn').style.display ="none"
-    document.querySelector('.regNewUser').style.display ="block"
-    document.querySelector('.existingUser').style.display ="none"
-}
-
-// button callback
-submitEl.addEventListener('click', pushObj);
-
-regNewUserEl.addEventListener('click', newUserinit)
-
-existingUserEl.addEventListener('click', existingLogin)
-
-loginEl.addEventListener('click', checkAccLogin)
-
-
-
-
-console.log(localStorage)
-console.log(users)
-
-// object creation
-// class UserObj {
-//     constructor(userName, userPassword){
-//         this.userName = userName
-//         this.userPassword = userPassword
-//     }
-// }
 
 // // app functions
 
@@ -116,10 +41,72 @@ console.log(users)
 
 // console.log(userNameEl, userPasswordEl);
 
-// submit element
 
-// create new user objects
+// Object Class Prototypes
+class UserObj {
+  constructor(){
+  this.userEmail = document.querySelector('.userEmail--Input').value 
+  this.userName = document.querySelector('.userName--Input').value
+  this.userPassword = document.querySelector('.userPassword--Input').value
+}
+}
 
-// display new users
+/////////////////////////// Button Methods
 
-// save users in the console
+// Login existing user/ check for account
+const checkAccLogin = function() {
+  formEl.remove();
+  console.log('logged in');
+  headerEl.append(`\n Welcome Back`);
+}
+
+// create User Object
+const pushObj = function() {
+  // user oject created
+  const newAccount = new UserObj()
+  // pushed to user array
+  users.push(newAccount);
+  formEl.remove();
+  headerEl.append(`\n Welcome ${newAccount.userName}`);
+  console.log('added', { newAccount });
+  localStorage.setItem('accounts', JSON.stringify(users))
+};
+
+////////////////////// Hide/Show CSS display
+// Display for new User 
+const newUserinit = function(){ 
+    document.querySelector('.userEmailDiv').style.display = "block"
+    document.querySelector('.loginBtn').style.display ="none"
+    document.querySelector('.submitBtn').style.display ="inline-block"
+    document.querySelector('.regNewUser').style.display ="none"
+    document.querySelector('.existingUser').style.display ="block"
+}
+// Existing User Login Display
+const existingLogin = function(){
+    document.querySelector('.userEmailDiv').style.display = "none"
+    document.querySelector('.loginBtn').style.display ="inline-block"
+    document.querySelector('.submitBtn').style.display ="none"
+    document.querySelector('.regNewUser').style.display ="block"
+    document.querySelector('.existingUser').style.display ="none"
+}
+
+///////////////////////////// button callback
+
+// Submit New User to Accounts
+submitEl.addEventListener('click', pushObj);
+
+// Change CSS to New User
+regNewUserEl.addEventListener('click', newUserinit)
+
+// Change CSS to Existing User Login
+existingUserEl.addEventListener('click', existingLogin)
+
+// Login Existing User 
+loginEl.addEventListener('click', checkAccLogin)
+
+
+
+
+console.log(localStorage)
+console.log(users)
+
